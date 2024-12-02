e31ee7b1003243f95a5be42f060055c27b6e8204
Coding920 <justin.tamez04@gmail.com>
2024-12-02 00:38:03 -0800

    Program currently runs, needs cleaning up.
    It only functions in the sense that it runs, it
    doesn't yet save the information yet
    
    I now have to figure out how to save the info in a
    file to read from and write to

--- Contactbook.py
+++ Contactbook.py
@@ -1,28 +1,43 @@
+#with open("Contactbook.txt", 'r+') as contactfile:
+#    contactbook = contactfile.read()
+#    pass
+
+contactbook = {}
+
 def main():
     print("Hello! You can: Add a new contact (add), list contacts (list), search contacts (search), quit (q)")
     
     while True:
             choice = input("What would you like to do? ")
 
-            match choice:
+            match choice.lower():
                 case "add":
-                    #todo add func
-                    return
+                    add()
 
                 case "list":
-                      #todo list func
-                      return
+                      lister()
 
                 case "search":
-                      #todo search func
-                      return
+                      search()
 
-                case "q":
-                      #todo end prgrm
-                      return
+                case "quit" | "q":
+                      break
                  
 def add():
      name = input("Who is it that you are adding? ")
      number = input("And their phone number? ")
 
-     
+     contactbook[f"{name}"] = f"{number}"
+
+def lister():
+     print(f"{contactbook.items()}")
+
+def search():
+     name = input("Who are you looking for? ")
+
+     if name in contactbook:
+          print(f"{contactbook[f"{name}"]}")
+     else:
+          print("Not found")
+
+main()
