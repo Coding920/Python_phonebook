031286c75b0f200e912a7fb1f7fbb43e809893be
Coding920 <justin.tamez04@gmail.com>
2024-12-01 23:19:04 -0800

    Starting file for contactbook program, unfinshed

---
+++ Contactbook.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello! You can: Add a new contact (add), list contacts (list), search contacts (search), quit (q)")
+    
+    while True:
+            choice = input("What would you like to do? ")
+
+            match (choice)
