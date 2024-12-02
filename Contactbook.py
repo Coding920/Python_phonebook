85d7eb68be6ebfbb96563da79fcce6aca2a64316
Coding920 <justin.tamez04@gmail.com>
2024-12-01 23:36:33 -0800

    Another commit of non-completed code, unsure why this isn't showing up in github

--- Contactbook.py
+++ Contactbook.py
@@ -4,4 +4,25 @@ def main():
     while True:
             choice = input("What would you like to do? ")
 
-            match (choice)
+            match choice:
+                case "add":
+                    #todo add func
+                    return
+
+                case "list":
+                      #todo list func
+                      return
+
+                case "search":
+                      #todo search func
+                      return
+
+                case "q":
+                      #todo end prgrm
+                      return
+                 
+def add():
+     name = input("Who is it that you are adding? ")
+     number = input("And their phone number? ")
+
+     
