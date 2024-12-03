ac97e2bc2d59187e862604865beda52caf96e787
Coding920 <justin.tamez04@gmail.com>
2024-12-03 00:40:10 -0800

    Added csv file and have some basic file handling, although its not truly working at the moment
    I believe im handling the file into dict right at
    the start but the saving of the dict to the csv is
    not functional yet
    
    Also having to rework the list function

--- Contactbook.py
+++ Contactbook.py
@@ -1,8 +1,14 @@
-#with open("Contactbook.txt", 'r+') as contactfile:
-#    contactbook = contactfile.read()
-#    pass
+import csv
 
+fieldnames = ['Name', 'Number']
 contactbook = {}
+
+with open("Contactbook.csv", 'r', newline='') as contactfile:
+    contactreader = csv.DictReader(contactfile)
+
+    for line in contactreader:
+         contactbook = line
+
 
 def main():
     print("Hello! You can: Add a new contact (add), list contacts (list), search contacts (search), quit (q)")
@@ -20,17 +26,22 @@ def main():
                 case "search":
                       search()
 
+                case "save":
+                      save()
+
                 case "quit" | "q":
-                      break
+                    break
                  
 def add():
      name = input("Who is it that you are adding? ")
      number = input("And their phone number? ")
 
-     contactbook[f"{name}"] = f"{number}"
+     #contactbook["Name:"] = f"{name}"
+     #contactbook["Number:"] = f"{number}" #Needs redone
 
 def lister():
-     print(f"{contactbook.items()}")
+     for rows in contactbook:
+          print(f"{contactbook}")
 
 def search():
      name = input("Who are you looking for? ")
@@ -39,5 +50,13 @@ def search():
           print(f"{contactbook[f"{name}"]}")
      else:
           print("Not found")
+
+def save():
+     with open("Contactbook.csv", 'w', newline='') as contactfile:
+          csvwriter = csv.DictWriter(contactfile, fieldnames = fieldnames)
+
+          csvwriter.writeheader()
+          for row in contactbook:
+               csvwriter.writerow(row)
 
 main()
